a4ca6d26beba39952fd5a13b36ffa42bb5fc87fe
Juan Cruz Ortega <juancruzortegacoldorf@gmail.com>
2022-03-31 08:52:36 -0300

    Agregado de funciones

--- TPs/TP1/TP1.py
+++ TPs/TP1/TP1.py
@@ -4,26 +4,65 @@ import numpy as np
 import pandas as pd
 import random
 
-crossover:float = 0.75
-mutation :float = 0.05 
+crossoverProb :float = 0.75
+mutationProb :float = 0.05 
 coef=(2**30)-1
 
 binaryList = [0,1]
-pupulation=[]
+population =[]
 cycles=20
 #%%
 #DEFINE FUNCTIONS
 
-def objFunction(x:int):
+def binaryToDecimal(binary):
+    return sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
+
+def objFunction(individual:list):
+    x = binaryToDecimal(individual)
     return (x/coef)**2
+
+def fitnessFunction(individual,pop:list):
+    x = binaryToDecimal(individual)
+    decimalPopulation = list(map(binaryToDecimal,pop))
+    return (x/sum(decimalPopulation))
+
+def maxInPop(pop):
+    decimalPopulation = map(binaryToDecimal,pop)
+    return max(decimalPopulation)
+    
+
+def minInPop(pop):
+    decimalPopulation = map(binaryToDecimal,pop)
+    return min(decimalPopulation)
 
 def randomIndividial():
     individual = np.random.choice(binaryList, size = 30)
     return individual
 
-def initializePopulation(population:list):
-
-    return 
+def initializePopulation():
+    a = []
+    for i in range(10):
+        a.append(randomIndividial())
+    return a
     
 def decision(probability):
     return random.random() < probability
+
+def selection():
+    #ACA RULETA
+    return
+
+def crossover(ind1,ind2):
+    point = np.random.randint(1,28)
+    newInd1 = [val for inx,val in enumerate(ind1) if inx<point]
+    #PIENSO QUE LA MEJOR FORMA ES DIVIDIR EL IND1 y EL IND2 EN DOS PARTES, EL PRE Y EL POST E INTERCALARLOS.    
+
+def mutation():
+    return
+
+#%%
+population = initializePopulation()
+
+decimalPopulation = map(binaryToDecimal,population)
+
+print(list(decimalPopulation))
