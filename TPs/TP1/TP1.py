4e086871767b786046f1e86debb419041e34c124
Juan Cruz Ortega <juancruzortegacoldorf@gmail.com>
2022-04-01 17:09:48 -0300

    logica general terminada

--- TPs/TP1/TP1.py
+++ TPs/TP1/TP1.py
@@ -1,68 +1,102 @@
-#%%
-#IMPORT LIBRARIES AND DEFINE COMMON VARIABLES
+# %%
+# IMPORT LIBRARIES AND DEFINE COMMON VARIABLES
 import numpy as np
 import pandas as pd
 import random
+from itertools import repeat
 
-crossoverProb :float = 0.75
-mutationProb :float = 0.05 
-coef=(2**30)-1
+crossoverProb: float = 0.75
+mutationProb: float = 0.05
+coef = (2**30)-1
 
-binaryList = [0,1]
-population =[]
-cycles=20
-#%%
-#DEFINE FUNCTIONS
+binaryList = [0, 1]
+population = []
+cycles = 20
+# %%
+# DEFINE FUNCTIONS
+
 
 def binaryToDecimal(binary):
     return sum(val*(2**idx) for idx, val in enumerate(reversed(binary)))
 
-def objFunction(individual:list):
+
+def objFunction(individual: list):
     x = binaryToDecimal(individual)
     return (x/coef)**2
 
-def fitnessFunction(individual,pop:list):
-    x = binaryToDecimal(individual)
-    decimalPopulation = list(map(binaryToDecimal,pop))
-    return (x/sum(decimalPopulation))
+
+def fitnessFunction(pop: list, individual):
+    x = objFunction(individual)
+    objFuncPopulation = list(map(objFunction, pop))
+    return (x/sum(objFuncPopulation))
+
 
 def maxInPop(pop):
-    decimalPopulation = map(binaryToDecimal,pop)
-    return max(decimalPopulation)
-    
+    objFuncPopulation = list(map(objFunction, pop))
+    return max(objFuncPopulation)
+
 
 def minInPop(pop):
-    decimalPopulation = map(binaryToDecimal,pop)
-    return min(decimalPopulation)
+    objFuncPopulation = map(objFunction, pop)
+    return min(objFuncPopulation)
+
 
 def randomIndividial():
-    individual = np.random.choice(binaryList, size = 30)
+    individual = np.random.choice(binaryList, size=30)
     return individual
+
 
 def initializePopulation():
     a = []
     for i in range(10):
         a.append(randomIndividial())
     return a
-    
+
+
 def decision(probability):
     return random.random() < probability
 
-def selection():
-    #ACA RULETA
-    return
 
-def crossover(ind1,ind2):
-    point = np.random.randint(1,28)
-    newInd1 = [val for inx,val in enumerate(ind1) if inx<point]
-    #PIENSO QUE LA MEJOR FORMA ES DIVIDIR EL IND1 y EL IND2 EN DOS PARTES, EL PRE Y EL POST E INTERCALARLOS.    
+def selection(pop):
+    inxPop = [inx for inx, val in enumerate(pop)]
+    inxParents = np.random.choice(a=inxPop, p=list(
+        map(fitnessFunction, repeat(population), population)), size=10)
+    parents = []
+    for x in inxParents:
+        parents.append(pop[x])
+    return parents
 
-def mutation():
-    return
 
-#%%
+def crossover(ind1, ind2):
+    point = np.random.randint(0, 28)
+    newInd1 = [gen for inx, gen in enumerate(ind1) if inx <= point]
+    newInd2 = [gen for inx, gen in enumerate(ind2) if inx <= point]
+    newInd1.extend([gen for inx, gen in enumerate(ind2) if inx > point])
+    newInd2.extend([gen for inx, gen in enumerate(ind1) if inx > point])
+
+    return newInd1, newInd2
+
+
+def mutation(ind):
+    point = np.random.randint(0, 29)
+    if ind[point] == 0:
+        ind[point] = 1
+    else:
+        ind[point] = 0
+    return ind
+
+
+# %%
 population = initializePopulation()
 
-decimalPopulation = map(binaryToDecimal,population)
+decimalPopulation = map(binaryToDecimal, population)
 
 print(list(decimalPopulation))
+# %%
+fitnessList = list(map(fitnessFunction, repeat(population), population))
+print(fitnessList)
+
+parents = selection(population)
+
+print(list(map(binaryToDecimal, parents)))
+# %%
