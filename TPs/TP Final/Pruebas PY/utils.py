97292d862ddd5b1ea98f90dad6ab4e9ae9ea44ac
Juan Cruz Ortega <juancruzortegacoldorf@gmail.com>
2022-08-02 09:48:14 -0300

    ss

--- TPs/TP Final/Pruebas PY/utils.py
+++ TPs/TP Final/Pruebas PY/utils.py
@@ -3,11 +3,17 @@ import numpy as np
 
 
 #Tabla Vientos - Potencia
-d = {'viento': np.arange(0,26), 'potencia': [0,0,0,0,53,106,166,252,350,464,560,630,660,660,660,660,660,660,660,660,660,660,660,660,660,0]}
+d = {'viento': np.arange(0,26), 'potencia': [0,0,0,0,53,106,166,252,350,464,560,630,660,660,660,660,660,660,660,660,660,660,660,660,660,0]} # SE APAGA POR PRECUACION 
 df = pd.DataFrame(data=d)
 #print(df)
 
 #Random Binary Matrix
+seedMatrix = [1 if x <= 24 else 0 for x in range(100)]
+ 
+print (seedMatrix)
+
+individual = np.random.permutation(seedMatrix).reshape(10,10)
+
 
 #------------------------------------ # Viento Este
 individual = np.mod(np.random.permutation(10*10).reshape(10,10),2)
@@ -21,9 +27,9 @@ Pensando funcion Fitness
  -> [0 1 1 1 0 1 0 1 0 0]       * Loop por fila iniciando velocidad del viento U(0)
  -> [0 1 1 0 0 1 0 0 1 0]       * El viento es diferente para cada una de las filas
  -> [0 0 0 0 0 0 1 0 0 0]       * El efecto estela altera al que esta directamente detras (¿cada vez que hay un 0 se resetea?)
- -> [1 0 1 0 1 0 1 1 0 0]
- -> [0 1 1 1 1 0 1 1 0 0]
- -> [1 1 0 0 1 1 1 1 1 1]
+ -> [1 0 1 0 1 0 1 1 0 0]       * TERRENO DE QUE TAMAÑO VA A SER (CANTIDAD DE CELDAS)
+ -> [0 1 1 1 1 0 1 1 0 0]       * TE PODES PASAR CUANDO MUTAS o CRUZAS DE LOS 25 AEROGENERADORES
+ -> [1 1 0 0 1 1 1 1 1 1]       * 
  -> [0 0 0 1 1 1 0 1 1 0]
  -> [0 1 1 0 0 0 0 1 0 0]
  -> [0 1 1 1 0 1 0 1 1 0]
